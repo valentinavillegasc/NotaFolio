2b6188f3531e3fe422f0faf2f42e9616e1e45f1e
valentinavillegasc <valentinavillegasc@hotmail.com>
2023-09-06 17:55:02 -0500

    AddNote component

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddNote() {
+  const [noteText, setNoteText] = useState("");
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+  /* const handleSaveClick=()=>{} */
   return (
-    <div className="note-new">
-      <textarea name="" id="" cols="10" rows="8"></textarea>
+    <div className="note new">
+      <textarea
+        placeholder="Type to add a note..."
+        id=""
+        cols="10"
+        rows="8"
+        value={noteText}
+        onChange={handleChange}></textarea>
+      <div className="note-footer">
+        <small>200 Remainig</small>
+        <button className="save" /* onClick={handleSaveClick} */>Save</button>
+      </div>
     </div>
   );
 }
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Note from "./Note";
-
+import AddNote from "./AddNote";
 export default function NotesList({ notes }) {
   return (
     <div className="notes-list">
       {notes.map((note) => (
         <Note key={note.id} id={note.id} text={note.text} date={note.date} />
       ))}
+      <AddNote />
     </div>
   );
 }
